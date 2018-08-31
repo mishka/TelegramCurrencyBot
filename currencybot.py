1019a8fa402a074238bb770545dbb6177b04f094
浪人 <mighxi@yahoo.com>
2018-08-31 22:18:39 +0300

    v0.5

--- currencybot.py
+++ currencybot.py
@@ -1,25 +1,31 @@
 # -*- coding: utf-8 -*-
 def main():
-    import re, requests
+    import re
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
     from bs4 import BeautifulSoup
     from datetime import datetime
 
     time = datetime.now().strftime('%H:%M')
     print(time + "\n")
+
     consumer_key = ""
     consumer_secret = ""
     access_token = ""
     access_token_secret = ""
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
+    
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
 
     print("Fetching gbp data..")
-    gbp_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=TRY", headers=headers)
-    gbp_data = gbp_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=TRY")
+    gbp_data = driver.page_source
     gbp_soup = BeautifulSoup(gbp_data, 'html.parser')
-    gbp_rate = gbp_soup.find('span', {'class' : 'uccResultAmount'})
+    gbp_rate = gbp_soup.find('span', {'class' : 'converterresult-toAmount'})
     gbp_current = re.sub('[^0-9,.]', '', str(gbp_rate))
     gbp_tweet = gbp_current[:-3]
-
+    
     with open("gbp.txt", "r+") as txt:
         old = txt.readlines()
         old = re.sub('[^.0-9]', '', str(old))
@@ -45,13 +51,13 @@ def main():
     gbp_final = "🇬🇧 £1 = ₺{} > {}\n".format(gbp_tweet, well)
 
     print("Fetching eur data..")
-    eur_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=EUR&To=TRY", headers=headers)
-    eur_data = eur_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=EUR&To=TRY")
+    eur_data = driver.page_source
     eur_soup = BeautifulSoup(eur_data, 'html.parser')
-    eur_rate = eur_soup.find('span', {'class' : 'uccResultAmount'})
+    eur_rate = eur_soup.find('span', {'class' : 'converterresult-toAmount'})
     eur_current = re.sub('[^0-9,.]', '', str(eur_rate))
     eur_tweet = eur_current[:-3]
-
+    
     with open("eur.txt", "r+") as txt:
         old = txt.readlines()
         old = re.sub('[^.0-9]', '', str(old))
@@ -77,12 +83,13 @@ def main():
     eur_final = "🇪🇺 €1 = ₺{} > {}\n".format(eur_tweet, well)
 
     print("Fetching usd data..")
-    usd_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=TRY", headers=headers)
-    usd_data = usd_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=TRY")
+    usd_data = driver.page_source
     usd_soup = BeautifulSoup(usd_data, 'html.parser')
-    usd_rate = usd_soup.find('span', {'class' : 'uccResultAmount'})
+    usd_rate = usd_soup.find('span', {'class' : 'converterresult-toAmount'})
     usd_current = re.sub('[^0-9,.]', '', str(usd_rate))
     usd_tweet = usd_current[:-3]
+    
 
     with open("usd.txt", "r+") as txt:
         old = txt.readlines()
@@ -109,10 +116,10 @@ def main():
     usd_final = "🇺🇸 $1 = ₺{} > {}\n".format(usd_tweet, well)
 
     print("Fetching ils data..")
-    ils_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ILS&To=TRY", headers=headers)
-    ils_data = ils_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ILS&To=TRY")
+    ils_data = driver.page_source
     ils_soup = BeautifulSoup(ils_data, 'html.parser')
-    ils_rate = ils_soup.find('span', {'class' : 'uccResultAmount'})
+    ils_rate = ils_soup.find('span', {'class' : 'converterresult-toAmount'})
     ils_current = re.sub('[^0-9,.]', '', str(ils_rate))
     ils_tweet = ils_current[:-3]
 
@@ -141,13 +148,13 @@ def main():
     ils_final = "🇮🇱 ₪1 = ₺{} > {}\n".format(ils_tweet, well)
 
     print("Fetching rub data..")
-    rub_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=RUB&To=TRY", headers=headers)
-    rub_data = rub_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=RUB&To=TRY")
+    rub_data = driver.page_source
     rub_soup = BeautifulSoup(rub_data, 'html.parser')
-    rub_rate = rub_soup.find('span', {'class' : 'uccResultAmount'})
+    rub_rate = rub_soup.find('span', {'class' : 'converterresult-toAmount'})
     rub_current = re.sub('[^0-9,.]', '', str(rub_rate))
     rub_tweet = rub_current[:-4]
-
+    
     with open("rub.txt", "r+") as txt:
         old = txt.readlines()
         old = re.sub('[^.0-9]', '', str(old))
@@ -173,13 +180,13 @@ def main():
     rub_final = "🇷🇺 ₽1 = ₺{} > {}\n".format(rub_tweet, well)
 
     print("Fetching cny data..")
-    cny_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=CNY&To=TRY", headers=headers)
-    cny_data = cny_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=CNY&To=TRY")
+    cny_data = driver.page_source
     cny_soup = BeautifulSoup(cny_data, 'html.parser')
-    cny_rate = cny_soup.find('span', {'class' : 'uccResultAmount'})
+    cny_rate = cny_soup.find('span', {'class' : 'converterresult-toAmount'})
     cny_current = re.sub('[^0-9,.]', '', str(cny_rate))
     cny_tweet = cny_current[:-4]
-
+    
     with open("cny.txt", "r+") as txt:
         old = txt.readlines()
         old = re.sub('[^.0-9]', '', str(old))
@@ -205,13 +212,13 @@ def main():
     cny_final = "🇨🇳 ¥1 = ₺{} > {}\n".format(cny_tweet, well)
 
     print("Fetching jpy data..")
-    jpy_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=JPY&To=TRY", headers=headers)
-    jpy_data = jpy_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=JPY&To=TRY")
+    jpy_data = driver.page_source
     jpy_soup = BeautifulSoup(jpy_data, 'html.parser')
-    jpy_rate = jpy_soup.find('span', {'class' : 'uccResultAmount'})
+    jpy_rate = jpy_soup.find('span', {'class' : 'converterresult-toAmount'})
     jpy_current = re.sub('[^0-9,.]', '', str(jpy_rate))
     jpy_tweet = jpy_current[:-4]
-
+    
     with open("jpy.txt", "r+") as txt:
         old = txt.readlines()
         old = re.sub('[^.0-9]', '', str(old))
@@ -237,13 +244,13 @@ def main():
     jpy_final = "🇯🇵 ¥1 = ₺{} > {}\n".format(jpy_tweet, well)
 
     print("Fetching krw data..")
-    krw_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=KRW&To=TRY", headers=headers)
-    krw_data = krw_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=KRW&To=TRY")
+    krw_data = driver.page_source
     krw_soup = BeautifulSoup(krw_data, 'html.parser')
-    krw_rate = krw_soup.find('span', {'class' : 'uccResultAmount'})
+    krw_rate = krw_soup.find('span', {'class' : 'converterresult-toAmount'})
     krw_current = re.sub('[^0-9,.]', '', str(krw_rate))
     krw_tweet = krw_current[:-4]
-
+    
     with open("krw.txt", "r+") as txt:
         old = txt.readlines()
         old = re.sub('[^.0-9]', '', str(old))
@@ -269,12 +276,13 @@ def main():
     krw_final = "🇰🇷 ₩1 = ₺{} > {}\n".format(krw_tweet, well)
 
     print("Fetching isk data..")
-    isk_url = requests.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ISK&To=TRY", headers=headers)
-    isk_data = isk_url.content
+    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ISK&To=TRY")
+    isk_data = driver.page_source
     isk_soup = BeautifulSoup(isk_data, 'html.parser')
-    isk_rate = isk_soup.find('span', {'class' : 'uccResultAmount'})
+    isk_rate = isk_soup.find('span', {'class' : 'converterresult-toAmount'})
     isk_current = re.sub('[^0-9,.]', '', str(isk_rate))
     isk_tweet = isk_current[:-4]
+    driver.quit()
 
     with open("isk.txt", "r+") as txt:
         old = txt.readlines()
@@ -306,8 +314,7 @@ def main():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
-    print("{}\n\n{}{}{}{}{}{}{}{}{}".format(time, gbp_final, eur_final, usd_final, ils_final, cny_final, rub_final, isk_final, jpy_final, krw_final))
-
+    print(f"{time}\n\n{gbp_final}{eur_final}{usd_final}{ils_final}{cny_final}{rub_final}{isk_final}{jpy_final}{krw_final}")
     api.update_status(status="{}\n\n{}{}{}{}{}{}{}{}{}".format(time, gbp_final, eur_final, usd_final, ils_final, cny_final, rub_final, isk_final, jpy_final, krw_final))
     print("Tweet sent!")
 
@@ -321,11 +328,11 @@ while True:
             main()
             print("Done, sleeping for 15 minutes.")
             import time
-            time.sleep(900)
+            time.sleep(1800)
         else:
             print("hibernating..")
             import time
-            time.sleep(900)
+            time.sleep(5000)
     except ValueError:
         import time
         time.sleep(30)
