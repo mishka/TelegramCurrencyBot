bb58b3591d2b7911c89c75b33a88f72285afbebe
浪人 <mighxi@yahoo.com>
2019-02-06 00:04:50 +0300

    v0.8
    shrunk it below 200 lines from 400+.

--- currencybot_xe.py
+++ currencybot_xe.py
@@ -1,95 +1,72 @@
 # -*- coding: utf-8 -*-
-import re, datetime
+import re, tweepy, datetime
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-
-timex = datetime.datetime.now().strftime('%H:%M')
-print(timex + "\n")
-
-chrome_options = Options()
-chrome_options.add_argument("--headless")
 
 consumer_key = ""
 consumer_secret = ""
 access_token = ""
 access_token_secret = ""
 
-def tweet():
-    import tweepy
+timex = datetime.datetime.now().strftime('%H:%M')
+print(timex + "\n")
 
+def tweet():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
     
-    datax = get_data()
-
-    api.update_status(status = datax)
+    api.update_status(status = get_data())
     print("Tweet sent!")
 
-def get_data():
-    gbpx = gbp()
-    eurx = eur()
-    usdx = usd()
-    ilsx = ils()
-    cnyx = cny()
-    rubx = rub()
-    iskx = isk()
-    jpyx = jpy()
-    krwx = krw()
+def browser(url):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    driver.quit()
+    rate = soup.find('span', {'class' : 'converterresult-toAmount'})
+    return re.sub('[^0-9,.]', '', str(rate))
 
-    data = "{}\n\n{}{}{}{}{}{}{}{}{}".format(timex, gbpx, eurx, usdx, ilsx, cnyx, rubx, iskx, jpyx, krwx) 
-    print(data)
-    return data
+def perc(x):
+    if "-" in x:
+        well = '📉 ' + x[:5] + '%'
+    else:
+        well = '📈 ' + x[:4] + '%'
+    return well
+
+def compare(old, current):
+    percentage = 100 * (float(current) - float(old)) / float(old)
+    list = []
+    list.append(percentage)
+    return ''.join(str(e) for e in list)
+
+def get_data():
+    return "{}\n\n{}{}{}{}{}{}{}{}{}".format(timex, gbp(), eur(), usd(), ils(), cny(), rub(), isk(), jpy(), krw()) 
 
 def gbp():
     print("Fetching gbp data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=TRY")
-    gbp_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    gbp_rate = gbp_soup.find('span', {'class' : 'converterresult-toAmount'})
-    gbp_current = re.sub('[^0-9,.]', '', str(gbp_rate))
-    gbp_tweet = gbp_current[:-3]
+    gbp_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=GBP&To=TRY")
     
     with open("gbp.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
     with open("gbp.txt", "r+") as txt:
         txt.write(gbp_current)
         txt.close()
-
-    current = float(gbp_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
-
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    gbp_final = "🇬🇧 £1 = ₺{} > {}\n".format(gbp_tweet, well)
-    return gbp_final
+    
+    return "🇬🇧 £1 = ₺{} > {}\n".format(gbp_current[:-3], perc(compare(old, gbp_current)))
 
 def eur():
     print("Fetching eur data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=EUR&To=TRY")
-    eur_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    eur_rate = eur_soup.find('span', {'class' : 'converterresult-toAmount'})
-    eur_current = re.sub('[^0-9,.]', '', str(eur_rate))
-    eur_tweet = eur_current[:-3]
+    eur_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=EUR&To=TRY")
     
     with open("eur.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -97,35 +74,13 @@ def eur():
         txt.write(eur_current)
         txt.close()
 
-    current = float(eur_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
+    return "🇪🇺 €1 = ₺{} > {}\n".format(eur_current[:-3], perc(compare(old, eur_current)))
 
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    eur_final = "🇪🇺 €1 = ₺{} > {}\n".format(eur_tweet, well)
-    return eur_final
-
-def usd():    
-    print("Fetching usd data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=TRY")
-    usd_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    usd_rate = usd_soup.find('span', {'class' : 'converterresult-toAmount'})
-    usd_current = re.sub('[^0-9,.]', '', str(usd_rate))
-    usd_tweet = usd_current[:-3]
-    
+def usd():
+    usd_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=USD&To=TRY")
 
     with open("usd.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -133,34 +88,13 @@ def usd():
         txt.write(usd_current)
         txt.close()
 
-    current = float(usd_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
+    return "🇺🇸 $1 = ₺{} > {}\n".format(usd_current[:-3], perc(compare(old, usd_current)))
 
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    usd_final = "🇺🇸 $1 = ₺{} > {}\n".format(usd_tweet, well)
-    return usd_final
-
-def ils():    
-    print("Fetching ils data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ILS&To=TRY")
-    ils_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    ils_rate = ils_soup.find('span', {'class' : 'converterresult-toAmount'})
-    ils_current = re.sub('[^0-9,.]', '', str(ils_rate))
-    ils_tweet = ils_current[:-3]
+def ils():
+    ils_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ILS&To=TRY")
 
     with open("ils.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -168,34 +102,13 @@ def ils():
         txt.write(ils_current)
         txt.close()
 
-    current = float(ils_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
+    return "🇮🇱 ₪1 = ₺{} > {}\n".format(ils_current[:-3], perc(compare(old, ils_current)))
 
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    ils_final = "🇮🇱 ₪1 = ₺{} > {}\n".format(ils_tweet, well)
-    return ils_final
-
-def rub():    
-    print("Fetching rub data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=RUB&To=TRY")
-    rub_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    rub_rate = rub_soup.find('span', {'class' : 'converterresult-toAmount'})
-    rub_current = re.sub('[^0-9,.]', '', str(rub_rate))
-    rub_tweet = rub_current[:-4]
+def rub():
+    rub_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=RUB&To=TRY")
     
     with open("rub.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -203,34 +116,14 @@ def rub():
         txt.write(rub_current)
         txt.close()
 
-    current = float(rub_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
-
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    rub_final = "🇷🇺 ₽1 = ₺{} > {}\n".format(rub_tweet, well)
-    return rub_final
+    return "🇷🇺 ₽1 = ₺{} > {}\n".format(rub_current[:-4], perc(compare(old, rub_current)))
 
 def cny():    
     print("Fetching cny data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=CNY&To=TRY")
-    cny_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    cny_rate = cny_soup.find('span', {'class' : 'converterresult-toAmount'})
-    cny_current = re.sub('[^0-9,.]', '', str(cny_rate))
-    cny_tweet = cny_current[:-4]
+    cny_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=CNY&To=TRY")
     
     with open("cny.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -238,34 +131,14 @@ def cny():
         txt.write(cny_current)
         txt.close()
 
-    current = float(cny_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
-
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    cny_final = "🇨🇳 ¥1 = ₺{} > {}\n".format(cny_tweet, well)
-    return cny_final
+    return "🇨🇳 ¥1 = ₺{} > {}\n".format(cny_current[:-4], perc(compare(old, cny_current)))
 
 def jpy():
     print("Fetching jpy data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=JPY&To=TRY")
-    jpy_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    jpy_rate = jpy_soup.find('span', {'class' : 'converterresult-toAmount'})
-    jpy_current = re.sub('[^0-9,.]', '', str(jpy_rate))
-    jpy_tweet = jpy_current[:-4]
-    
+    jpy_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=JPY&To=TRY")
+
     with open("jpy.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -273,34 +146,14 @@ def jpy():
         txt.write(jpy_current)
         txt.close()
 
-    current = float(jpy_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
-
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    jpy_final = "🇯🇵 ¥1 = ₺{} > {}\n".format(jpy_tweet, well)
-    return jpy_final
+    return "🇯🇵 ¥1 = ₺{} > {}\n".format(jpy_current[:-4], perc(compare(old, jpy_current)))
 
 def krw():
     print("Fetching krw data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=KRW&To=TRY")
-    krw_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    krw_rate = krw_soup.find('span', {'class' : 'converterresult-toAmount'})
-    krw_current = re.sub('[^0-9,.]', '', str(krw_rate))
-    krw_tweet = krw_current[:-4]
+    krw_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=KRW&To=TRY")
     
     with open("krw.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -308,34 +161,14 @@ def krw():
         txt.write(krw_current)
         txt.close()
 
-    current = float(krw_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
-
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    krw_final = "🇰🇷 ₩1 = ₺{} > {}\n".format(krw_tweet, well)
-    return krw_final
+    return "🇰🇷 ₩1 = ₺{} > {}\n".format(krw_current[:-4], perc(compare(old, krw_current)))
 
 def isk():
     print("Fetching isk data..")
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-    driver.get("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ISK&To=TRY")
-    isk_soup = BeautifulSoup(driver.page_source, 'html.parser')
-    driver.quit()
-    isk_rate = isk_soup.find('span', {'class' : 'converterresult-toAmount'})
-    isk_current = re.sub('[^0-9,.]', '', str(isk_rate))
-    isk_tweet = isk_current[:-4]
+    isk_current = browser("https://www.xe.com/currencyconverter/convert/?Amount=1&From=ISK&To=TRY")
 
     with open("isk.txt", "r+") as txt:
-        old = txt.readlines()
-        old = re.sub('[^.0-9]', '', str(old))
+        old = re.sub('[^.0-9]', '', str(txt.readlines()))
         txt.truncate(0)
         txt.close()
 
@@ -343,20 +176,7 @@ def isk():
         txt.write(isk_current)
         txt.close()
 
-    current = float(isk_current)
-    old = float(old)
-    percentage = 100 * (current - old) / old
-    list = []
-    list.append(percentage)
-    string = ''.join(str(e) for e in list)
-
-    if "-" in string:
-        well = '📉 ' + string[:5] + '%'
-    else:
-        well = '📈 ' + string[:4] + '%'
-
-    isk_final = "🇮🇸 kr1 = ₺{} > {}\n".format(isk_tweet, well)
-    return isk_final
+    return "🇮🇸 kr1 = ₺{} > {}\n".format(isk_current[:-4],perc(compare(old, isk_current)))
 
 while True:
     day_of_week = datetime.date.today().weekday()
